8fa0c2272b02801b495a48613509f6ef20ad5bb4
ironytr <ironys@outlook.com>
2023-01-14 08:36:49 +0300

    remove dist

--- dist/module.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "C:/Users/iRoNY/Desktop/projects/nuxt3-socket.io/src/module";
-export { default } from "C:/Users/iRoNY/Desktop/projects/nuxt3-socket.io/src/module";
--- dist/module.mjs
+++
@@ -1,7 +0,0 @@
-import jiti from "file:///C:/Users/iRoNY/Desktop/projects/nuxt3-socket.io/node_modules/.pnpm/jiti@1.16.0/node_modules/jiti/lib/index.js";
-
-/** @type {import("C:/Users/iRoNY/Desktop/projects/nuxt3-socket.io/src/module")} */
-const _module = jiti(null, { interopDefault: true, esmResolve: true })("C:/Users/iRoNY/Desktop/projects/nuxt3-socket.io/src/module.ts");
-
-export default _module;
-export const defineIOHandler = _module.defineIOHandler;
